fcab6be849afccab886faae45553fc3930e65f5b
oliverfeher <oliver92.feher@gmail.com>
2020-03-13 00:23:44 -0400

    Done.

---
+++ db/migrate/20200313030609_create_schoolclass_table.rb
@@ -0,0 +1,8 @@
+class CreateSchoolclassTable < ActiveRecord::Migration[5.0]
+  def change
+    create_table :schoolclass do |t|
+      t.string :title
+      t.integer :room_number
+    end
+  end
+end
